b010fda879b73680d218f2dbe4603349a724cead
Rynilan <ilanvitorsantos@gmail.com>
2024-12-24 15:50:13 -0300

    code: adicionado código de criação do banco e inserção de registros ao sistema.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- model/banco.sql
+++ model/banco.sql
@@ -1,12 +1,18 @@
 create database Pokedex;
-create table Pokedex.tb_pokemons(
+create table Pokedex.tb_tipos(
+	id int (10) not null auto_increment primary key ,
+	nome varchar(10) not null
+);
+create table Pokedex.tb_pokemons (
 	id int(10) not null auto_increment primary key,
 	nome varchar(50) not null,
-	descricao varchar(500) not null,
-	tipo_1 varchar(10) not null,
-	tipo_2 varchar(10),
-	forca int(3) not null,
-	defesa int(3) not null,
-	hp int(3) not null,
-	foto varchar(200) not null
+	tipo_1 int(10) not null,
+	tipo_2 int(10),
+	foto varchar(100) not null,
+	defesa int not null,
+	hp int not null,
+	ataque int not null,
+	descrição varchar(500),
+	foreign key (tipo_2) references tb_tipos(id),
+	foreign key (tipo_1) references tb_tipos(id)
 );
