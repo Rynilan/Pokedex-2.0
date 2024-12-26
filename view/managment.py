68c33e66382c3ea5e88105b02bd943efd12667fa
Rynilan <ilanvitorsantos@gmail.com>
2024-12-26 13:36:25 -0300

    code: adicionado código para mostrar a tabela e as informações dos registros.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- view/managment.py
+++ view/managment.py
@@ -1,5 +1,6 @@
 from view.tela import Tela
-from tkinter import Toplevel, Tk, Label, Entry, Button, Frame, Text, ttk, END
+from tkinter import (Toplevel, Tk, Label, Entry, Button, Frame, Text,
+                     Scrollbar, ttk, END)
 from model.crud import campos, conectar, tipos, regioes, select_join
 
 
@@ -10,6 +11,8 @@ class Managment(Tela):
         top.geometry('720x680')
         top.title('Gerenciamento de pokemons')
         self.top = top
+        self.sair = Button(top, command=top.destroy, text='Sair')
+        self.sair.pack(side='bottom', fill='x')
 
         # Testa a conexão com o banco.
         try:
@@ -20,6 +23,7 @@ class Managment(Tela):
                                  'eu com a\nconexão com o banco de dados.')
             return
 
+        # Criação dos campos para inserção de dados do usuário.
         frame_campos = Frame(top)
         frame_campos.place(x=10, y=10, relwidth=0.45, relheight=0.9)
         self.frame_campos = frame_campos
@@ -47,6 +51,7 @@ class Managment(Tela):
         campos_widgets.get('campo descrição').pack()
         self.campos = campos
 
+        # Criação dos botões parqa as ações especificadas.
         frame_acao = Frame(top, bg='white')
         frame_acao.place(relx=0.55, y=10, relwidth=0.45, relheight=0.2)
         self.registrar = Button(frame_acao, text='Registrar',
@@ -58,6 +63,34 @@ class Managment(Tela):
         self.excluir = Button(frame_acao, text='Excluir',
                               command=self.delete)
         self.excluir.pack(fill='x')
+        self.pesquisa = Button(frame_acao, text='Pesquisar',
+                               command=self.pesquisar)
+        self.pesquisa.pack(fill='x')
+
+        # Criação da tabela e suas barras de rolagem.
+        self.frame_tabela = Frame(self.top)
+        self.frame_tabela.place(relx=0.55,
+                                rely=0.25,
+                                relheight=0.7,
+                                relwidth=0.45)
+        self.tabela = ttk.Treeview()
+        self.yscroll: Scrollbar = Scrollbar(self.frame_tabela,
+                                            orient='vertical')
+        self.yscroll.place(relx=0.9, rely=0, relwidth=0.05, relheight=0.9)
+        self.xscroll: Scrollbar = Scrollbar(self.frame_tabela,
+                                            orient='horizontal')
+        self.xscroll.place(relx=0, rely=0.95, relwidth=0.9, relheight=0.04)
+        self.tabela: ttk.Treeview = ttk.Treeview(
+            self.frame_tabela,
+            show='headings',
+            selectmode='browse',
+            yscrollcommand=self.yscroll.set,
+            xscrollcommand=self.xscroll.set,
+            columns=campos())
+        self.tabela.place(relx=0, rely=0, relheight=0.92, relwidth=0.9)
+        self.yscroll.configure(command=self.tabela.yview)
+        self.xscroll.configure(command=self.tabela.xview)
+        self.por_colunas()
 
     def gerar_tabela(self: object) -> None:
         pass
@@ -78,12 +111,15 @@ class Managment(Tela):
     def delete(self: object) -> None:
         pass
 
+    def pesquisar(self: object) -> None:
+        pass
+
     def por_colunas(self: object) -> None:
         ''' Método que irá por na tabela os mesmos registros existentes
             no banco de dados nesse GUI. '''
         tabela = self.tabela
-        tabela.heading('id', text='Id')
-        tabela.heading('personagem', text='Personagem')
+        for campo in campos():
+            tabela.heading(campo.lower(), text=campo.capitalize())
 
         for elemento in select_join():
             tabela.insert('', END, values=elemento)
@@ -97,5 +133,6 @@ class Managment(Tela):
             selectmode='browse',
             yscrollcommand=self.tabela_scroll.set,
             columns=campos())
-        self.tabela.place(relx=0, rely=0, relheight=0.9, relwidth=0.95)
+        self.tabela.configure(yscrollcommand=self.tabela_scroll.set)
+        self.tabela.place(relx=0, rely=0, relheight=1, relwidth=0.9)
         self.por_colunas()
