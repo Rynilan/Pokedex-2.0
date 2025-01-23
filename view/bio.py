949940c7c41cf364742c2debdf3a9d3e966ffd01
Rynilan <ilanvitorsantos@gmail.com>
2025-01-23 18:52:03 -0300

    raw: added the photo of the pokemons.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- view/bio.py
+++ view/bio.py
@@ -8,7 +8,7 @@ from mysql.connector import connect
 def tela_de_biografia(numero: int):
     banco = connect(host='localhost', user='root', password='', database='Pokedex')
     cursor = banco.cursor()
-    cursor.execute('select * from tb_pokemons where id = ' + str(numero))
+    cursor.execute('select * from tb_pokemons where numero_geral = ' + str(numero))
     dados = cursor.fetchall()
     cursor.close()
     banco.close()
@@ -22,7 +22,7 @@ def tela_de_biografia(numero: int):
     frame.place(relx=0.1,rely=0.23,relheight=0.5)
 
     # Título centralizado
-    
+
     titulo = tk.Label(janela, text="𝚋𝚒𝚘𝚐𝚛𝚊𝚏𝚒𝚊", font=("Helvetica",34), fg="black", bg="red")
     janela.config(bg="red")
     titulo.pack(pady=20)  # Adiciona título com margem
@@ -41,9 +41,9 @@ def tela_de_biografia(numero: int):
     # Texto da biografia alinhado à direita
     biografia_texto = list(dados[0])
     biografia_texto.pop(4)
-    pp = tk.Label(janela,text=str(biografia_texto[0])+biografia_texto[1],fg='black',bg='white',font=("Consolas",30))
+    pp = tk.Label(janela,text=str(biografia_texto[0])+str(biografia_texto[1]),fg='black',bg='white',font=("Consolas",30))
     pp.place(relx=0.38,rely=0.2)
-    pp = tk.Label(janela,text="𝚃𝙸𝙿𝙾:"+str(biografia_texto[2])+biografia_texto[3],fg='black',bg='white',font=("Corbel",20))
+    pp = tk.Label(janela,text="𝚃𝙸𝙿𝙾:"+str(biografia_texto[2])+str(biografia_texto[3]),fg='black',bg='white',font=("Corbel",20))
     pp.place(relx=0.38,rely=0.3)
     pp = tk.Label(janela,text="𝙳𝙴𝙵𝙴𝚂𝙰:"+str(biografia_texto[4]),fg='black',bg='white',font=("Corbel",20))
     pp.place(relx=0.38,rely=0.4)
@@ -57,17 +57,18 @@ def tela_de_biografia(numero: int):
     fechar_frame = tk.Frame(janela, bg="black")  # Criar um frame com fundo preto
     fechar_frame.pack(side="bottom", pady=20, fill="x")  # Posicionar na parte inferior, ocupando toda a largura
 
+    def primeiro(tela_biografia,pokemon_atual):
+        tela_biografia.destroy()
+        tela_biografia(1)
+
     # Botão "Fechar" centralizado dentro do frame
     fechar_btn = tk.Button(fechar_frame, text="Fechar", command=janela.quit, font=("Helvetica", 14))
     fechar_btn.pack()  # Centraliza o botão dentro do frame
     ultima=tk.Button(janela,text='ultimo',command=ultimo)
     ultima.pack()
 
-    primeiro= tk.Button(tela_biografia,text='primeiro',command=primeiro)
-    primeiro.pack()
-    def primeiro(tela_biografia,pokemon_atual):
-        tela_biografia.destroy()
-        tela_biografia(1)
+    primeira= tk.Button(janela,text='primeiro',command=primeiro)
+    primeira.pack()
 
     # Executar a janela
     janela.mainloop()
@@ -75,8 +76,8 @@ def tela_de_biografia(numero: int):
 def ultimo (janela,pokemon_atual):
     janela.destroy()
 
-    tela_biografia(151)
+    tela_de_biografia(151)
 
 # Chama a função para mostrar a janela
 if __name__ == '__main__':
-    tela_biografia(1)
+    tela_de_biografia(1)
